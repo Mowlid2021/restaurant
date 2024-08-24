3e5082b16cd9070784f9bf86d0cd71f48ced9c91
Your Name <your.email@example.com>
2024-08-24 09:46:34 +0300

    Isbeddel cusub oo la sameeyey

--- src/index.jsx
+++ src/index.jsx
@@ -2,8 +2,7 @@ import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { ShopProvider } from './contex/ShopContext.jsx'
-
+import { ShopProvider } from './contex/shopContext.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
 
 <React.StrictMode>
